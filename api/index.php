5deba40b5ba7561f0e2a7d15f36654eaae75a9a1
Tenderness-Miao <miao@miaohao.sn.cn>
2021-04-26 00:04:11 +0800

    update alarm template +

--- api/index.php
+++ api/index.php
@@ -3,15 +3,15 @@ require_once 'Bot.php';
 
 header('content-type: application/json');
 $arr = json_decode(file_get_contents('php://input'), true);
-$alarmId = "告警编号" . " " . $arr['alarmId'];
-$creationTime = "告警时间" . " " . $arr['creationTime'];
-$priority = "告警级别" . " " . $arr['priority'];
-$alarmName = "告警标题" . " " . $arr['alarmName'];
-$alarmContent = "告警内容" . " " . $arr['alarmContent'];
-$entityName = "告警对象" . " " . $arr['entityName'];
-$app = "告警应用" . " " . $arr['app'];
-$count = "告警次数" . " " . $arr['count'];
-$status = "告警状态" . " " . $arr['status'];
+$alarmId = "告警编号:" . "\t" . $arr['alarmId'];
+$creationTime = "告警时间:" . "\t" . $arr['creationTime'];
+$priority = "告警级别:" . "\t" . $arr['priority'];
+$alarmName = "告警标题:" . "\t" . $arr['alarmName'];
+$alarmContent = "告警内容:" . "\t" . $arr['alarmContent'];
+$entityName = "告警对象:" . "\t" . $arr['entityName'];
+$app = "告警应用:" . "\t" . $arr['app'];
+$count = "告警次数:" . "\t" . $arr['count'];
+$status = "告警状态:" . "\t" . $arr['status'];
 
 $data = $alarmId . "\n" . $creationTime . "\n" . $priority . "\n" . $alarmName . "\n" . $alarmContent . "\n" . $entityName . "\n" . $app . "\n" . $count . "\n" . $status;
 $token = $_REQUEST['token'] ?? null;
