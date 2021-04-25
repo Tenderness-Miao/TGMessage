696083f19d0896a3366afe6b412e7d4a9eddfae6
Tenderness-Miao <miao@miaohao.sn.cn>
2021-04-26 00:01:28 +0800

    update alarm template

--- api/index.php
+++ api/index.php
@@ -3,8 +3,19 @@ require_once 'Bot.php';
 
 header('content-type: application/json');
 $arr = json_decode(file_get_contents('php://input'), true);
+$alarmId = "告警编号" . " " . $arr['alarmId'];
+$creationTime = "告警时间" . " " . $arr['creationTime'];
+$priority = "告警级别" . " " . $arr['priority'];
+$alarmName = "告警标题" . " " . $arr['alarmName'];
+$alarmContent = "告警内容" . " " . $arr['alarmContent'];
+$entityName = "告警对象" . " " . $arr['entityName'];
+$app = "告警应用" . " " . $arr['app'];
+$count = "告警次数" . " " . $arr['count'];
+$status = "告警状态" . " " . $arr['status'];
+
+$data = $alarmId . "\n" . $creationTime . "\n" . $priority . "\n" . $alarmName . "\n" . $alarmContent . "\n" . $entityName . "\n" . $app . "\n" . $count . "\n" . $status;
 $token = $_REQUEST['token'] ?? null;
-$message = $_REQUEST['message'] ?? $arr['alarmContent'];
+$message = $_REQUEST['message'] ?? $data;
 
 
 $bot = new Bot();
